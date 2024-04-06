73ef5d5f8926f27994fdcf262d9dd4bdb4d14d68
Vika <GurevaVS@yandex.ru>
2024-04-06 17:07:23 +0300

    все, кроме пагинации

--- app/urls.py
+++ app/urls.py
@@ -8,5 +8,11 @@ from app import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hot/', views.hot, name='hot'),
-    path('questions/<int:question_id>', views.question, name='question')
+    path('questions/<int:question_id>', views.question, name='question'),
+    path('ask', views.ask, name='ask'),
+    path('settings', views.settings, name='settings'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('tag/<str:tag_name>', views.tag, name='tag'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -10,18 +10,64 @@ QUESTIONS = [
         "text": f"This is question number {i}"
     } for i in range(200)
 ]
-
+POPULAR = {
+    "tags": [f"tag{i+1}" for i in range(5)],
+    "users": [f"user{i+1}" for i in range(5)]
+}
 
 
 def index(request):
     page_num = request.GET.get('page', 1)
     paginator = Paginator(QUESTIONS, 5)
     page_obj = paginator.page(page_num)
-    return render(request, "index.html", {"questions": page_obj})
+    return render(request, "index.html", {"current": "New Questions",
+                                          "other": "Hot Questions",
+                                          "questions": page_obj,
+                                          "popular": POPULAR})
 
 def hot(request):
-        return render(request,"hot.html", {"questions": QUESTIONS})
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(QUESTIONS, 5)
+    page_obj = paginator.page(page_num)
+    return render(request,"hot.html", {"current": "Hot Questions",
+                                            "other": "New Questions",
+                                            "questions": page_obj,
+                                            "popular": POPULAR})
 
 def question(request, question_id):
     item = QUESTIONS[question_id]
     return render(request,"question_detail.html", {"question": item})
+
+
+def ask(request):
+    return render(request, "ask.html", {"page_title": "Ask", "popular": POPULAR})
+
+
+def settings(request):
+    context = {
+        "page_title": "Settings",
+        "settings": {
+            "current_login": "Dr. Pepper",
+            "current_email": "example@mail.com",
+            "current_NickName": "VIKA_Guryeva"
+        },
+        "popular": POPULAR
+    }
+    return render(request, 'settings.html', context)
+
+
+def register(request):
+    return render(request, "register.html", {"page_title": "Registration", "popular": POPULAR})
+
+
+def login(request):
+    return render(request, 'login.html', {"page_title": "Login", "popular": POPULAR})
+
+def tag(request, tag_name):
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(QUESTIONS, 5)
+    page_obj = paginator.page(page_num)
+
+    return render(request, "tag.html", {"page_title": f"Tag: {tag_name}",
+                                        "questions": page_obj,
+                                        "popular": POPULAR})
